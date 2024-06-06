96033eeb351539893fc6be3056f651e01a6dfc29
sashapoliti <sas.politi@gmail.com>
2024-06-06 21:07:46 +0200

    fix store validation

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -22,10 +22,21 @@ class StoreProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|max:200|min:3|unique:projects,title',
-            'image' => 'max:255',
-            'description' => 'required',
+            'title' => 'required|max:200|min:3|unique:projects',
+            'image' => 'nullable|max:255',
+            'description' => 'nullable',
             'type_id' => 'nullable|exists:types,id'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.unique' => 'Title already exists',
+            'title.min' => 'Title must be at least :min characters',
+            'title.max' => 'Title must be at most :max characters',
+            'title.required' => 'Title is required',
+            'image.max' => 'Image must be at most :max characters',
         ];
     }
 }
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,7 +6,7 @@
     <h1>{{$project->title}}</h1>
 
     <p>{{$project->content}}</p>
-    <img src="{{$project->image}}" alt="{{$project->title}}">
+    <img src="{{$project->image}}" alt="Image of {{$project->title}}">
     <p>{{$project->type ? $project->type->name : 'No type'}}</p>
 </section>
 @endsection
